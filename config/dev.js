eadc5efea60e1a2e2359709f49f6a8b4b2751957
Srinivasa L <srinil16@in.ibm.com>
2021-03-16 19:02:25 +0530

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+    googleClientSecret: 'knH-rZC23z4fr2CNISK4srN',
+    mongoURI: 'mongodb://localhost/mern_react',
+    cookieKey: 'mern',
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+  };
